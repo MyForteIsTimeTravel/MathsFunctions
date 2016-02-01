ff1306f75eaf5c6976710ab8dd2dcce3e2e69e04
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-02-01 14:20:29 +0000

    Added modulo function

--- MathsFunctions.cpp
+++ MathsFunctions.cpp
@@ -2,3 +2,4 @@ int add (int a, int b) { return a + b; }
 int sub (int a, int b) { return a - b; }
 int mul (int a, int b) { return a * b; }
 int div (int a, int b) { return a / b; }
+int remainder (int a, int b) {return a % b; }
