f659283298820880a9660378ec13ebcbb0c2952b
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-02-01 14:58:57 +0000

    Added min/max code

--- MathsFunctions.cpp
+++ MathsFunctions.cpp
@@ -1,5 +1,46 @@
-int add (int a, int b) { return a + b; }
-int sub (int a, int b) { return a - b; }
-int mul (int a, int b) { return a * b; }
-int div (int a, int b) { return a / b; }
-int remainder (int a, int b) {return a % b; }
+#include <iostream>
+
+int addition  (int a, int b) { return a + b; }
+int subtract  (int a, int b) { return a - b; }
+int multiply  (int a, int b) { return a * b; }
+int divide    (int a, int b) { return a / b; }
+int remainder (int a, int b) { return a % b; }
+
+int main (void) {
+    int a, b, c, d, e;
+    int max, min;
+    
+    std::cout << "Enter 1 integer: ";
+    std::cin  >> a;
+    
+    max = a;
+    min = a;
+    
+    std::cout << "Enter 1 integer: ";
+    std::cin  >> b;
+    
+    if (b > max) { max = b; }
+    if (b < min) { min = b; }
+    
+    std::cout << "Enter 1 integer: ";
+    std::cin  >> c;
+    
+    if (c > max) { max = c; }
+    if (c < min) { min = c; }
+        
+    std::cout << "Enter 1 integer: ";
+    std::cin  >> d;
+    
+    if (d > max) { max = d; }
+    if (d < min) { min = d; }
+    
+    std::cout << "Enter 1 integer: ";
+    std::cin  >> e;
+    
+    if (e > max) { max = e; }
+    if (e < min) { min = e; }
+    
+    std::cout << "minimum: " << min << std::endl;
+    std::cout << "maximum: " << max << std::endl;
+    
+}
